c2291851eb045cae582c4cc93a4b00e6652b824f
gaungalif <gaungalif@gmail.com>
2023-12-06 11:43:23 +0700

    module

---
+++ Day 2/module/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World, index.js")
+// const {coffeeStock, isCoffeeMachineReady} = require('./state');
+// import {coffeeStock, isCoffeeMachineReady} from './state.js';
+// const coffeeStock = require('./state');
+
+// console.log(coffeeStock);
+// const displayStock = stock => {
+//     for (const type in stock) {
+//       console.log(type);
+//     }
+//   }
+   
+// displayStock(coffeeStock);
---
+++ Day 2/module/state.js
@@ -0,0 +1,10 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    excelsa: 112
+}
+module.exports = coffeeStock;
+   
+// const isCoffeeMachineReady = true;
+// export {coffeeStock, isCoffeeMachineReady}
+// module.exports = {coffeeStock, isCoffeeMachineReady};
