842a5a7e37a05747d1a28652e2b51b27d31824da
gaungalif <gaungalif@gmail.com>
2023-12-07 13:57:14 +0700

    node package module

---
+++ Day 3/npm/lodash.js
@@ -0,0 +1,14 @@
+import _ from "lodash"
+
+
+const myArray = [1, 2, 3, 4]
+let newArray = _.concat(7,23)
+// display newArray in lodash
+console.log(newArray)
+
+// let sum = 0
+// for (let i = 0; i < myArray.length; i++) {
+//   sum += myArray[i]
+// }
+
+// console.log(sum)
